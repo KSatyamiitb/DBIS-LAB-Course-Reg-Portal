8de586df3b55379e9e58f523f680c488621e2a07
Tj1503 <chukkatanuja15@gmail.com>
2023-02-12 00:47:43 +0530

    getCurrent time and year

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -5,17 +5,21 @@ const saltRounds = 10
 // const { sign } = require('jsonwebtoken')
 // const { SECRET } = require('../constants')
 
-const getCurYear = async () => {
+const getCurrent = async () => {
   try{
-    let curtime = Date.now()
-    console.log(curtime)
-    await db.query('select year,semester from reg_dates ')
-
-    return res.status(200)
+    var result = await db.query('select * from section order by year desc,case when semester = \'Spring\' then 1 when semester = \'Summer\' then 2 when semester = \'Fall\' then 3 else 4 end desc limit 1')
+    console.log(result)
+    console.log("1111")
+    console.log(result.rows)
+    console.log(result.rows[0])
+    console.log(result.rows[0].semester)
+    console.log(result.rows[0].year)
+    return result.rows[0]
   } catch (error) {
     console.log(error.message)
   }
 }
+
 
 exports.getUsers = async (req, res) => {
   try {
@@ -100,8 +104,9 @@ exports.dropcourse = async (req, res) => {
 
 exports.runningdept = async (req, res) => {
   try {
-    var cur_year = 2010
-    var cur_sem = 'Spring'
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
     var result = await db.query('select distinct dept_name from section natural join course where year = $1 and semester = $2',[cur_year, cur_sem])
     // console.log(result.rows)
     let payload = {
@@ -117,11 +122,12 @@ exports.runningdept = async (req, res) => {
 
 exports.runningdeptcourses = async (req, res) => {
   try {
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
     var dept = "%"
     dept = dept.concat(req.originalUrl.split("/")[3].split("%")[0],"%")
     // console.log(dept)
-    var cur_year = 2010
-    var cur_sem = 'Spring'
     var result = await db.query('select distinct course_id, title from section natural join course where year = $1 and semester = $2 and dept_name ilike $3',[cur_year, cur_sem, dept])
     // console.log(result.rows)
     let payload = {
@@ -137,13 +143,14 @@ exports.runningdeptcourses = async (req, res) => {
 
 exports.getcourse = async (req, res) => {
   try {
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
     // function f() {
     //   return dom.useParams()
     // }
     // var x = f()
     // console.log(x)
-    var cur_year = 2010
-    var cur_sem = 'Spring'
     var course_id = req.originalUrl.split("/")[2]
     // console.log(course_id)
     var result = await db.query('select * from course where course_id = $1',[course_id])
@@ -165,8 +172,11 @@ exports.getcourse = async (req, res) => {
 
 exports.getinstructor = async (req, res) => {
   try {
-    var cur_year = 2010
-    var cur_sem = 'Spring'
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
+    // var cur_year = 2010
+    // var cur_sem = 'Spring'
     // console.log(req.originalUrl.split("/"))
     var id = req.originalUrl.split("/")[3]
     // console.log(id)
@@ -189,8 +199,11 @@ exports.getinstructor = async (req, res) => {
 
 exports.registration = async (req, res) => {
   try {
-    var cur_year = 2010
-    var cur_sem = 'Spring'
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
+    // var cur_year = 2010
+    // var cur_sem = 'Spring'
     var result = await db.query('select * from student where ID = $1',[req.user.id])
     var result2 = await db.query('select * from takes where ID = $1 and ( year < $2 or (year = $2 and semester < $3))  ORDER BY year desc, semester desc',[req.user.id, cur_year, cur_sem])
     var result3 = await db.query('select course_id from takes where ID = $1 and year = $2 and semester = $3',[req.user.id, cur_year, cur_sem])
@@ -222,10 +235,13 @@ exports.registration = async (req, res) => {
 
 //Newly adde function
 exports.registerForCourse = async (req, res) => {
-  var cur_year = 2010
-  var cur_sem = 'Spring'
+  // var cur_year = 2010
+  // var cur_sem = 'Spring'
   var { id, course_id, sec_id, title } = req.body
   try {
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
     console.log(req.body)
     
     var prereq =await db.query('select prereq_id from prereq where prereq_id not in (select course_id from takes where ID=$1 and (year<$2 or (year=$2 and semester!=$3))) and course_id=$4',[id,cur_year,cur_sem,course_id])
@@ -262,15 +278,18 @@ exports.registerForCourse = async (req, res) => {
 
 exports.protected = async (req, res) => {
   try {
-    var cur_year = 2010
-    var cur_sem = 'Spring'
+    const cur = await getCurrent()
+    const cur_sem = cur.semester
+    const cur_year = cur.year
+    // var cur_year = 2010
+    // var cur_sem = 'Spring'
     var first_sem='Spring'
     var second_sem='Summer'
     var third_sem='Fall'
     var result = await db.query('select * from student where ID = $1',[req.user.id])
     var result2 = await db.query('select * from takes where ID = $1 and ( year < $2 or (year = $2 and semester < $3))  ORDER BY year desc, semester desc',[req.user.id, cur_year, cur_sem])
     var result3 = await db.query('select * from takes where ID = $1 and year = $2 and semester = $3',[req.user.id, cur_year, cur_sem])
-    var result4 = await db.query('select year,semester, array_agg(id||\',\'||course_id||\',\'||title||\',\'||sec_id||\',\'||grade ) as rows from takes natural join course where ID=$1 and (year<$2 or (year=$2 and semester!=$3)) group by year,semester order by year desc,case when semester=\'Spring\' then 1 when semester=\'Summer\' then 2 when semester=\'Fall\' then 3 else 4 end asc ',[req.user.id,cur_year,cur_sem])
+    var result4 = await db.query('select year,semester, array_agg(id||\',\'||course_id||\',\'||title||\',\'||sec_id||\',\'||grade ) as rows from takes natural join course where ID=$1 and (year<$2 or (year=$2 and semester!=$3)) group by year,semester order by year desc,case when semester=\'Spring\' then 1 when semester=\'Summer\' then 2 when semester=\'Fall\' then 3 else 4 end desc ',[req.user.id,cur_year,cur_sem])
 
     console.log(result4.rows)
     
